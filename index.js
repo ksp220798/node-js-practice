b273384c6f48975a01fe7c0e7237e49c6b7cc501
ksp220798 <70259929+ksp220798@users.noreply.github.com>
2025-12-06 19:21:31 +0530

    Add files via upload
    check..

---
+++ index.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req,res)=>{
+   // console.log(req);
+    if(req.url == '/check'){
+        // const filePath = path.join(__dirname, 'index.html');
+        // fs.readFile(filePath,(err,data)=>{
+        //     if(err){
+        //         res.statusCode(400);
+        //         res.end("something went wrong");
+        //     }else{
+        //         res.write(data);
+        //         res.write("<h2> hii i am from another thing</h2>")
+        //         res.end();
+        //     }
+        // })
+        res.end("hii i am from API");
+    }
+
+});
+
+server.listen(300,()=>{ console.log("server runnig")
+
+
+    const dd = http.get('http://localhost:300/check',(res)=>{
+    let ds= "";
+  //  console.log("check",res);
+     res.on('data',(value)=>{
+        ds+=value;
+    })
+    res.on('end',()=>{
+     console.log("i send request",ds);
+    })
+})
+});
+
+
+
